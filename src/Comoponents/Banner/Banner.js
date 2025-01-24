16a593257224b10bf656f79349cced18d7a1a0fe
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2025-01-24 05:43:07 -0500

    axios seting

--- src/Comoponents/Banner/Banner.js
+++ src/Comoponents/Banner/Banner.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from '../../axios'
 import  './banner.css'
 function Banner() {
+  useEffect(() => {
+    axios.get('trending/movie/week?language=en-US&api_key=12b30436e8cbc461e1a11c11613a2491') 
+   
+  }, [])
+  
+ 
+  
+
   return (
     <div className='banner'>
 
@@ -8,7 +17,7 @@ function Banner() {
          <h1  className='title'>Movie Name</h1>
          <div className='banner_button' >
             <button className='button' >play</button>
-            <button className='button' >Mylist</button>
+            <button className='button' >Mylist</button>r
          </div>
          <h1 className='discription'>t looks like you’re no longer using the Ubersuggest Chrome extension. We’ve automatically disabled it. You can turn it back on quickly by clicking the “Unhide” button.</h1>
 
---
+++ src/Constants/constant.js
@@ -0,0 +1,2 @@
+export const baseUrl='https://www.themoviedb.org/3';
+export const API_Key='12b30436e8cbc461e1a11c11613a2491';
